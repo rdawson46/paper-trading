ca5497c1fbb9111a7cf4755e617b1484381dd329
crooksryan <rdc5410@psu.edu>
2023-05-08 01:08:39 -0400

    initial files

---
+++ README.md
@@ -0,0 +1,1 @@
+# Web App for Paper Trading
---
+++ application.py
@@ -0,0 +1,9 @@
+import os
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+socketio = SocketIO(app)
+
