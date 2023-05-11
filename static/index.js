bf3b00c1a01c9ae98594cc169100eded2baa6240
crooksryan <rdc5410@psu.edu>
2023-05-11 17:16:29 -0400

    removed typescript due to issues with client side socketIO

--- static/index.js
+++ static/index.js
@@ -1,9 +1,7 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var socket_io_client_1 = require("socket.io-client");
-addEventListener("DOMContentLoaded", function () {
-    var socket = (0, socket_io_client_1.io)();
-    socket.on('connect', function () {
+addEventListener("DOMContentLoaded", ()=>{
+    const socket = io();
+
+    socket.on('connect', ()=>{
         console.log('connected to socket');
-    });
+    })
 });
--- static/index.ts
+++
@@ -1,9 +0,0 @@
-import { io } from "socket.io-client";
-
-addEventListener("DOMContentLoaded", ()=>{
-    const socket = io();
-
-    socket.on('connect', ()=>{
-        console.log('connected to socket');
-    })
-})
