3c2bdd54b113df38422e298d09c7314ab2f70184
crooksryan <rdc5410@psu.edu>
2023-05-23 20:34:21 -0400

    added session variables for login, manage and settings page, dashboard styling, and routing features

--- application.py
+++ application.py
@@ -4,7 +4,8 @@ import os # used for getting enviroment variables set during execution
 import requests # find an api for getting stock prices, use .env file for hiding the api key
 from dotenv.main import load_dotenv
 
-from flask import Flask, render_template, request, jsonify, redirect, make_response
+from flask import Flask, render_template, request, jsonify, redirect, make_response, session
+from flask_session import Session
 from flask_socketio import SocketIO, emit
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -30,6 +31,7 @@ if not os.getenv('DATABASE_URL'):
 
 app.config['SESSION_PERMANENT'] = False
 app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
 
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
@@ -79,11 +81,17 @@ def register():
             return render_template('register.html', error="Email already in use")
 
         # add user to db
-        # get ID number
         # sign them to session token
-        # direct them to dashboard with their id
+        # direct them to dashboard with their username
         # also find a way to use the session token to keep the user logged in and prevent anyone else logging into dash via user id number
-        
+        db.execute(text('INSERT INTO users (username, email, pass) VALUES (:username, :email, :password)'), {'username': username, 'email':email, 'password': password1})
+        db.execute(text('INSERT INTO balances (username, balance) VALUES (:username, :balance)'), {'username': username, 'balance': 0})
+        db.commit()
+
+        session['username'] = username
+
+        return redirect(f'/dashboard/{username}')
+
     return render_template('register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -94,24 +102,63 @@ def login():
         email = request.form.get('email')
         password = request.form.get('password')
 
-        if db.execute(text("SELECT * FROM users WHERE email = :email AND pass = :pass"), {'email': email, 'pass':password}).rowcount == 0:
-            # fail case
-            ...
-        else:
-            # get ID number and load the dashboard and make the session token
-            return redirect('/dashboard/USER', 200)
+        result = db.execute(text("SELECT username FROM users WHERE email = :email AND pass = :pass"), {'email': email, 'pass':password})
+
+        if result.rowcount != 0:
+            # get username and load the dashboard and make the session token
+            result = result.fetchone()
+
+            user = result[0]
+            session['username'] = user
+
+            return redirect(f'/dashboard/{user}')
 
         # get from the db to check
     return render_template('login.html')
 
 @app.route('/dashboard/<string:user>')
 def dashboard(user):
-    # need to get account balance and account number
     # check to make sure the user is logged in when requested
-    return render_template('dashboard.html', user=user)
+    if not (username := session.get('username')):
+        return redirect('/')
 
-@app.route('/<string:user>/<string:name>')
-def stockDate(name):
+    if username != user:
+        return redirect('/')
+    
+    # need to get account balance and account number
+    balance = db.execute(text('SELECT balance from balances WHERE username = :username'), {'username':user}).fetchone()[0]
+    
+    # need to update balance
+    return render_template('dashboard.html', user=user, balance=balance)
+
+@app.route('/manage/<string:user>')
+def manage(user):
+    if not (username:=session.get('username')):
+        return redirect('/')
+
+    if username != user:
+        return redirect('/')
+    
+    return render_template('manage.html', user=user)
+
+@app.route('/settings/<string:user>')
+def settings(user):
+    if not (username:=session.get('username')):
+        return redirect('/')
+
+    if username != user:
+        return redirect('/')
+    
+    return render_template('settings.html', user=user)
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+
+    return redirect('/')
+
+@app.route('/<string:user>/<string:symbol>')
+def stockDate(symbol):
     # this function will be called for getting api requests
     ...
 
